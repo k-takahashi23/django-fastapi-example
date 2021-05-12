8684676d48b2b52f71aa5a53dc8fab8141051337
k-takahashi23 <efl.sad23@gmail.com>
2021-05-12 22:47:54 +0900

    ADD: users

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/users")
+async def users():
+    return {"users": [
+        {
+            "id": "id1",
+            "name": "Tanaka Taro"
+        },
+        {
+            "id": "id2",
+            "name": "Tanaka Jiro"
+        }
+    ]}
