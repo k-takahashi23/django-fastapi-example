e4c32e3816110e38726b0098b1f7d4cca59d9339
k-takahashi23 <efl.sad23@gmail.com>
2021-05-13 02:23:11 +0900

    MOD: use uuid

--- app/application/users/createUserUsecase.py
+++ app/application/users/createUserUsecase.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 from typing import Optional
 from pydantic import BaseModel
+import uuid
 from ...infrastructure.repositories.usersRepository import UsersRepository
 from ...domain.entities.user import User
 
@@ -19,7 +20,7 @@ class CreateUserUsecase:
         self.usersRepository = usersRepository
 
     async def invoke(self, req: CreateUserRequest):
-        user_id = "id"
+        user_id = str(uuid.uuid4())
         user = User(user_id, req.user_name, req.email)
         await self.usersRepository.addAsync(user)
         return user.__dict__
